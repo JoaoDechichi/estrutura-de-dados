0587246312865f228a9fe6e94236733dbcbe44a0
joao fernando <jf12medeiros@gmail.com>
2023-08-14 21:38:16 -0300

    EXEMPLOS

---
+++ output/VetorEX1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    //Declarando e inicializando um vetor de inteiros
+
+    int numeros[5] = {1, 2, 3, 4, 5};
+
+    //Declarando uma string (vetor de caracteres)
+
+    char  nome[10] = "Exemplo";
+
+    printf("\nTerceiro elemento do vetor: %d\n", numeros[2]);
+    printf("\nPrimeiro letra do nome: %c", nome[0]);
+    
+    return 0;
+}
