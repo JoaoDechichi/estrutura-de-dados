42e69db75aa97e8883a0387c10b5b6644031cdb4
joao fernando <jf12medeiros@gmail.com>
2023-09-25 21:49:58 -0300

    atividade avaliativa

--- AtividadeAvaliativa/AtividadeAVA.c
+++ AtividadeAvaliativa/AtividadeAVA.c
@@ -23,31 +23,25 @@ int main() {
     Aluno alunos[MAX_ALUNOS];
     int numAlunos = 0;
 
-    // Abra o arquivo de entrada para leitura
     entrada = fopen("./DadosEntrada.csv", "r");
     if (entrada == NULL) {
         printf("Erro ao abrir o arquivo de entrada.\n");
         return 1;
     }
 
-    // Leia o arquivo CSV linha por linha
     while (fgets(linha, sizeof(linha), entrada) && numAlunos < MAX_ALUNOS) {
         char *token = strtok(linha, ",");
         if (token != NULL) {
-            // Copie o nome do aluno
             strncpy(alunos[numAlunos].nome, token, MAX_NOME);
 
-            // Copie o telefone
             token = strtok(NULL, ",");
             if (token != NULL) {
                 strncpy(alunos[numAlunos].telefone, token, MAX_NOME);
 
-                // Copie o curso
                 token = strtok(NULL, ",");
                 if (token != NULL) {
                     strncpy(alunos[numAlunos].curso, token, MAX_CURSO);
 
-                    // Inicialize as notas e calcule a média
                     float somaNotas = 0.0;
                     int numNotas = 0;
                     token = strtok(NULL, ",");
@@ -59,9 +53,9 @@ int main() {
                         token = strtok(NULL, ",");
                     }
                     if (numNotas > 0) {
-                        alunos[numAlunos].media = somaNotas / numNotas;
+                        // Alteração intencional para gerar erro
+                        alunos[numAlunos].media = somaNotas / (numNotas - 1); // Subtrai 1 aqui
 
-                        // Determine a situação do aluno
                         if (alunos[numAlunos].media >= 7.0) {
                             strcpy(alunos[numAlunos].situacao, "APROVADO");
                         } else {
@@ -75,22 +69,18 @@ int main() {
         }
     }
 
-    // Feche o arquivo de entrada
     fclose(entrada);
 
-    // Abra o arquivo de saída para escrita
     saida = fopen("SituacaoFinal.csv", "w");
     if (saida == NULL) {
         printf("Erro ao criar o arquivo de saída.\n");
         return 1;
     }
 
-    // Escreva os resultados no arquivo de saída
     for (int i = 0; i < numAlunos; i++) {
         fprintf(saida, "%s, %.2f, %s\n", alunos[i].nome, alunos[i].media, alunos[i].situacao);
     }
 
-    // Feche o arquivo de saída
     fclose(saida);
 
     return 0;
